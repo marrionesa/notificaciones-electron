aaa1f4708eb3ae476daf8da154666ab5d97f2cb2
XnightmareX <37255107+marrionesa@users.noreply.github.com>
2021-08-20 21:41:35 +0200

    Create ejemplo0.js

---
+++ ejemplo0.js
@@ -0,0 +1,10 @@
+const notifier = require('node-notifier');
+
+// String
+notifier.notify('Nueva notificación de XNightmarex');
+
+// Objeto
+notifier.notify({
+  title: 'XNightmareX',
+  message: 'Sígueme en redes @marrionesa'
+});
